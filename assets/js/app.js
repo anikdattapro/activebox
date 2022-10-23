74bd638074d15c376bc187b66360d0c0f9d0abee
Anik Datta <anikdatta50@gmail.com>
2022-10-23 14:39:02 +0600

    activebox-psd-html

---
+++ assets/js/app.js
@@ -0,0 +1,22 @@
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    dots: true,
+    responsive:{
+        0:{
+            items:1,
+            nav: true
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+})
